91cfb5c187ec8fe0e72d6eaff71793aea88659f3
Ertholin <67572982+Ertholin@users.noreply.github.com>
2023-02-08 03:56:36 -0500

    Adding success and failure pages

--- app.js
+++ app.js
@@ -34,11 +34,17 @@ app.post("/", function(req, res){
 
   const options = {
     method: "POST",
-    auth: "ertho1:a45a0d52643b22485cc86119f448a56c-us18"
-
+    auth: "ertho1:315449f523b8523a20bc661637d314a5-us18"
   }
 
   const request = https.request(url, options, function(response){
+
+    if(response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+    } else {
+        res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function(data){
       console.log(JSON.parse(data));
     })
@@ -48,6 +54,10 @@ app.post("/", function(req, res){
   request.end();
 
 });
+
+app.post("/failure.html", function(req, res){
+  res.redirect("/");
+})
 
 
 app.listen(3000, function(){
