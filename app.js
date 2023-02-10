5aa017522a0dd85a940497437a180710611ffef9
Ertholin <67572982+Ertholin@users.noreply.github.com>
2023-02-10 17:55:26 -0500

    Initializing Heroku

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post("/failure.html", function(req, res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on Port 3000");
 })
